977f5bf9a811675b05be1813389ca54051c3a2bd
usr-undeleted <zephimhere@gmail.com>
2026-07-20 12:54:13 -0300

    expand on the exit command

--- src/shell.c
+++ src/shell.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 #include "defs.h"
 
 // main() opens a shell mode
@@ -14,7 +16,7 @@ void shell_help(void) {
     printf(
         "brainfuck shell basics:\n"
         "- commands:\n"
-        "\texit:  exit the program.\n"
+        "\texit:  exit the program. You may specify a second argument to return that specific number.\n"
         "\thelp:  show this help message.\n"
         "\tclear: clear the terminal.\n\n"
 
@@ -31,16 +33,30 @@ int shell(void) {
         write(STDOUT_FILENO, SHELL_PROMPT, sizeof(SHELL_PROMPT));
 
         // read user input into buffer
-        scanf("%s", buf);
+        //scanf("%s", buf);
+        fgets(buf, sizeof(buf), stdin);
 
         // form_buf() does buffer formation too, trough a fd.
         // i need to separate the digest part from it, making a separate function
         // to use here...
         // maybe make it take one const buf to copy from, and another to copy to
 
-        if      (!strcmp(buf, "exit"))  return 0;
-        else if (!strcmp(buf, "clear")) printf("\x1b[H\x1b[2J\x1b[J");
-        else if (!strcmp(buf, "help"))  shell_help();
+        if (!strncmp(buf, "exit", 4) &&
+            (isspace(buf[4]) || buf[4] == '\0')) {
+                // parse second argument
+                char *p = buf;
+                int ret = strtol(&buf[5], &p, 0);
+
+                // if the number provided isn't perfect, strtol
+                // doesn't move 'p' to the null terminator or whitespace
+                if (*p != '\0' && !isspace(*p)) {
+                    fprintf(stderr, "bfsh: imperfect parsing, stopped at \"%.*s\".\n", (int)strlen(p) - 1, p);
+                }
+
+                return ret;
+        }
+        else if (!strcmp(buf, "clear"    )) printf("\x1b[H\x1b[2J\x1b[J");
+        else if (!strcmp(buf, "help"     )) shell_help();
 
         interpreter(buf, strlen(buf));
         putchar('\n');
